11a34d511daf63e7bec3b8d54784c85b8935f18f
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-09 22:16:51 +0800

    I have finished the javaScript code, this one was very simple, but it always good for practcing.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imgJogo = gameClicado.querySelector('.dashboard__item__img');
+    let btnJogo = gameClicado.querySelector('.dashboard__item__button');
+    let nameJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if (btnJogo.classList.contains('dashboard__item__button--return') && imgJogo.classList.contains('dashboard__item__img--rented')) {
+        btnJogo.classList.remove('dashboard__item__button--return');
+        imgJogo.classList.remove('dashboard__item__img--rented');
+        btnJogo.textContent = 'Alugar';
+    } else {
+        btnJogo.classList.add('dashboard__item__button--return');
+        imgJogo.classList.add('dashboard__item__img--rented');
+        btnJogo.textContent = 'Devolver';
+    }
+}
