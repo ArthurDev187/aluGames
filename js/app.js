628838ca1d5e0dd71945e44e86ca50bd28985dc6
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-09 22:19:21 +0800

    Just deleting a unused variable

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imgJogo = gameClicado.querySelector('.dashboard__item__img');
     let btnJogo = gameClicado.querySelector('.dashboard__item__button');
-    let nameJogo = gameClicado.querySelector('.dashboard__item__name');
 
     if (btnJogo.classList.contains('dashboard__item__button--return') && imgJogo.classList.contains('dashboard__item__img--rented')) {
         btnJogo.classList.remove('dashboard__item__button--return');
